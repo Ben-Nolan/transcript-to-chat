6bf2548fbd76a6889315d600c524b87d9d7454fd
Ben Nolan <bcnolan34@gmail.com>
2026-01-03 20:12:16 -0500

    fix: Added ability to delete existing vectore store

--- utils/vectorestore.py
+++ utils/vectorestore.py
@@ -4,17 +4,34 @@ from langchain_core.vectorstores import VectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
+@st.cache_resource
+def get_embedding_model() -> HuggingFaceEmbeddings:
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+
+    return HuggingFaceEmbeddings(model_name=model_name)
+
+
 def create_vector_store(chunks: list[str]) -> VectorStore:
     """
     Convert chunks into vectors and store them in an in-memory ChromaDB
     """
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    embeddings = get_embedding_model()
+    COLLECTION_NAME = "youtube_transcript"
+
+    try:
+        old_store = Chroma(
+            collection_name=COLLECTION_NAME,
+            embedding_function=embeddings,
+        )
+        old_store.delete_collection()
+
+    except Exception:
+        pass
 
     vectore_store = Chroma.from_texts(
         texts=chunks,
         embedding=embeddings,
-        collection_name="youtube_transcript"
+        collection_name=COLLECTION_NAME
     )
 
     return vectore_store
